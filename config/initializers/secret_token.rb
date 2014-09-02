1e71e75f1e56fc5be290e37e88bcce4c7d7bc8af
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2014-09-01 21:01:20 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Comotti::Application.config.secret_key_base = 'ea749af46e6dbe9ee4e542f0163ce4de268d9c83ae9fb3a468d8e0287bc9f4f83dd35daf9cee97e9864764b1261392317aeab8f90b9ff30fa933015c9f7407ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Comotti::Application.config.session_store :cookie_store, key: '_comotti_session'
